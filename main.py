0aa0ac5579f6fd8f8823814a8e9b4f7e5e9aead8
jdubuisson <jdubuisson@bdsnet.com.ar>
2024-07-30 14:09:29 +0000

    clase 1

---
+++ main.py
@@ -0,0 +1,16 @@
+""" COMO IMPRIMIR ALGO EN PYTHON """
+
+""" print ("Hola Mundo")
+print ("Bienvenidos a S6")
+print ("Profesor: Julian Dubuisson")  """
+
+
+num1 = 5
+num2 = 2
+
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+print (num1 % num2)
+
