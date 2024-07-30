a738054cb8ed87f521b406e58d43ad696762aa6d
jdubuisson <jdubuisson@bdsnet.com.ar>
2024-07-30 11:43:26 +0000

    prueba

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import os
+a = 5
+b = 10
 
-from flask import Flask, send_file
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return send_file('src/index.html')
-
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
-
-if __name__ == "__main__":
-    main()
+print(a + b)
