5383de0769a0ef9e21f727679a32bfb8b8cba6ad
jdubuisson <jdubuisson@bdsnet.com.ar>
2024-08-06 11:01:18 +0000

    clase 2

---
+++ clase2.py
@@ -0,0 +1,47 @@
+"""Tipos de datos"""
+
+""" str = "Julian" (Cadena de texto)
+int = 1 (Número entero, no tiene decimales)
+float = 1.5 (Número con decimal)
+boolean = True or False (Permite saber si es verdadero o falso) """
+
+"""-------------------------------------------------------------------"""
+
+"""¿Cómo declarar una variable?"""
+
+""" nombre = "Julian"
+edad = 25
+peso = 83.7
+es_profesor = True
+es_estudiante = False """
+
+"""-------------------------------------------------------------------"""
+
+"""¿Cómo imprimir una variable?"""
+
+"""-------------------------------------------------------------------"""
+
+"""¿Cómo declarar una variable?"""
+
+
+"""-------------------------------------------------------------------"""
+
+"""¿Cómo imprimir una constante?"""
+
+
+"""-------------------------------------------------------------------"""
+
+"""Operadores de aritméticos"""
+
+""" a = 5
+b = 2 """
+
+""" print (a + b)""" """Suma"""
+"""print (a - b)""" """Resta"""
+"""print (a * b)""" """Multiplicación"""
+"""print (a / b)""" """División"""
+"""print (a % b)""" """Resto""" 
+
+"""-------------------------------------------------------------------"""
+
+"Inputs"
