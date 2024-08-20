a72c44d293ab0541f374d7245f11460dc342579f
jdubuisson <jdubuisson@bdsnet.com.ar>
2024-08-20 11:11:05 +0000

    actividad errores

---
+++ Clases/actividadError.py
@@ -0,0 +1,25 @@
+print("Hola, mundo!")
+
+num = 10
+
+print("El número es", num)
+
+suma = 5 + 3
+
+print(suma)
+
+
+nombre = "Juan"
+
+apellido = "Pérez"
+
+nombre_completo = nombre + apellido
+
+print(nombre_completo)
+
+
+#CODIGO 3
+
+edad = input("¿Cuántos años tienes? ")
+
+print("El año que viene tendrás", edad + 1, "años.")
--- Clases/clase2.py
+++ Clases/clase2.py
@@ -5,40 +5,63 @@ int = 1 (Número entero, no tiene decimales)
 float = 1.5 (Número con decimal)
 boolean = True or False (Permite saber si es verdadero o falso) """
 
-nombre = "Julian"
+""" nombre = "Julian"
+nombre = "Tomas"
 edad = 25
+edad = 10
 peso = 75.4
+peso = 80.5
 mayorEdad = True
-menorEdad = False
+menorEdad = False """
 
 """-------------------------------------------------------------------"""
 
 """¿Cómo imprimir una variable?"""
 
-print(nombre)
+""" print(nombre)
 print(edad)
 print(peso)
-print("Tu peso es", peso)
+print("Tu peso es", peso) """
+""" print(nombre, edad, peso, mayorEdad, menorEdad) """
 """-------------------------------------------------------------------"""
 
 """¿Cómo imprimir una constante?"""
-
+""" EDAD = 17
+print (EDAD) """
 
 """-------------------------------------------------------------------"""
 
 """Operadores de aritméticos"""
 
-""" a = 5
-b = 2 """
+""" a = 10
+b = 5
 
-""" print (a + b)""" """Suma"""
-"""print (a - b)""" """Resta"""
-"""print (a * b)""" """Multiplicación"""
-"""print (a / b)""" """División"""
-"""print (a % b)""" """Resto""" 
+print (a + b) 
+print (a - b) 
+print (a * b) 
+print (a / b) 
+print (a % b) 
+print (a ** b) 
+print (a // b)   """
 
-"""-------------------------------------------------------------------"""
+
+""" ------------------------------------------------------------------- """
 
 "Inputs"
 
 
+""" nombre = input ("¿Cual es tu nombre?")
+print ("Hola", nombre) 
+
+edad = int (input("¿Cual es tu edad?"))
+print ("Tu edad es", edad)
+
+peso = float(input("Ingresa tu peso"))
+print ("tu peso es", peso) """
+
+nombre = "pepito"
+apellido = "carrazco"
+
+nombre_completo = nombre + apellido
+
+print (nombre_completo)
