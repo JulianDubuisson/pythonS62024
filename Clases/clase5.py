2db29e26b6b059c6de80c3fd0836eb1a43f9dad6
jdubuisson <jdubuisson@bdsnet.com.ar>
2024-09-03 11:44:10 +0000

    clase 5

---
+++ Clases/clase5.py
@@ -0,0 +1,41 @@
+""" ensaldaFrutas = ["manzana", "melon", "arandanos"]
+                #   0         1          2
+print (ensaldaFrutas)
+
+"manzana" in ensaldaFrutas #Compruebo si esta la manzana en la lista
+
+ensaldaFrutas[0] = "pera" #Modifica el valor de la manzana
+
+ensaldaFrutas.append ("kiwi") #Agregue un valor al final de la lista
+
+ensaldaFrutas.insert(1, "frutilla") #Agregue un valor en el puesto 1 de la lista
+
+del(ensaldaFrutas[4]) #Elimino el 4 elemento de la lista (en este caso seria el kiwi)
+
+
+edad = 2
+
+if edad >= 18:
+    print("Sos mayor de edad, podes pasar")
+else:
+    print("Sos menor de edad, no podes pasar") """
+
+
+nota = 7
+
+if nota <= 1:
+    print("Reprobaste")
+elif nota <= 4:
+    print("Final")
+elif nota <= 7:
+    print("Aprobaste")
+else:
+    print("Tu nota no es correcta, acercate a preceptoria")
+
+
+numero = 10
+
+if numero >= 8:
+    print("Pasaste de ronda")
+else:
+    print("Perdiste")
