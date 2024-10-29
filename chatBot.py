ef22ca678ba6da679f1e9842da59e2769d5e6650
jdubuisson <jdubuisson@bdsnet.com.ar>
2024-10-29 14:10:38 +0000

    chatBot

---
+++ chatBot.py
@@ -0,0 +1,49 @@
+class ChatBot:
+    def __init__(self):
+        self.options ={
+            "inicio": {
+                "mensaje": "Bienvenidos a Bot Day School ¿En qué te puedo ayudar?",
+                "opciones": [
+                    "1. Secciones y Areas",
+                    "2. Admisiones",
+                    "3. Contacto",
+                    "4.Horario de atención"
+                ]    
+            },
+            "secciones":{
+                "1. Kinder",
+                "2. Primary",
+                "3. Middle",
+                "4. Senior",
+                "5. Volver al inicio"
+            },
+            "kinder": {
+                "1. Jornada",
+                "2. Edades",
+                "3. Actividades",
+                "4. Volver a Secciones y Áreas"
+                "5. Volver al menu principal"
+            },
+            "primary":{
+                "1. Jornada",
+                "2. Edades",
+                "3. Actividades",
+                "4. Volver a Secciones y Áreas"
+                "5. Volver al menu principal"
+            },
+            "middle":{
+                "1. Jornada",
+                "2. Edades",
+                "3. Actividades",
+                "4. Volver a Secciones y Áreas"
+                "5. Volver al menu principal"
+            },
+            "senior":{
+                "1. Jornada",
+                "2. Edades",
+                "3. Actividades",
+                "4. Volver a Secciones y Áreas"
+                "5. Volver al menu principal"
+            }
+            
+        }
